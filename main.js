32f75c02c7e8856fc0bcfaf431f3925ebee8abf0
sagnikghorai <63553430+sagnikghorai@users.noreply.github.com>
2020-11-13 20:39:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preLoad(){
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw () {
+    image(video, 0, 0, 640, 480);
+}
+
+function take_snapshot(){
+    save('homie.png');
+}

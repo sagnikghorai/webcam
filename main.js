900f7aabd97bceb57fd69361fb2c02f7efe7df3f
sagnikghorai <63553430+sagnikghorai@users.noreply.github.com>
2020-11-19 20:34:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-function preLoad(){
+noseX=0;
+noseY=0;
+
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
-function setup() {
-    canvas = createCanvas(640, 480);
-    canvas.position(150, 150);
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
     video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw () {
-    image(video, 0, 0, 640, 480);
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(noseX, noseY, 10);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
-    save('homie.png');
+    save('myFilterImage.png');
 }
